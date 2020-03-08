671c67872354ac459651dc480f7ac3f8c5fa207b
Marco Dalla Libera <info@marcodallalibera.it>
2020-03-08 17:35:57 +0100

    Fix linting

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,9 @@
 /**
-* File: module.ts
-* Author: Marco Dalla Libera
-* Creation date: 2020-02-19
-* Description: Main module file of the app plugin, that imports other resources
-*/
+ * File: module.ts
+ * Author: Marco Dalla Libera
+ * Creation date: 2020-02-19
+ * Description: Main module file of the app plugin, that imports other resources
+ */
 
 import { TestControl } from './components/config';
 import { AppPlugin } from '@grafana/data';
