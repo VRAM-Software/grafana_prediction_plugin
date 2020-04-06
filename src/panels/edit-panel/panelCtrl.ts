f0fee4cde2c62a387f48cf21cbadc2526eb0808e
Marco Dalla Libera <info@marcodallalibera.it>
2020-04-06 23:55:02 +0200

    Fix nodeMap, close #23

--- src/panels/edit-panel/panelCtrl.ts
+++ src/panels/edit-panel/panelCtrl.ts
@@ -21,6 +21,8 @@ export class PlotlyPanelCtrl extends MetricsPanelCtrl {
     predictionSettings: {
       version: null,
       json: null,
+      predictors: [],
+      queries: [],
       nodeMap: [],
       writeDatasourceID: '',
       influxHost: '',
@@ -30,7 +32,6 @@ export class PlotlyPanelCtrl extends MetricsPanelCtrl {
       influxFieldKey: '',
     },
   };
-  queryList: any[];
   plotlyPanelUtil: PlotlyPanelUtil;
   private _graphDiv: any;
   private predictionPanelConfig: any;
@@ -75,32 +76,42 @@ export class PlotlyPanelCtrl extends MetricsPanelCtrl {
   }
 
   // Called only on import button click, if the import doesn't throw errors, it reset the saved data
-  async upload_button_click(net: any) {
+  async uploadButtonClick(net: any) {
     await this.onUpload(net);
   }
 
   // Called on import button click but also to re-load a saved json
   async onUpload(net: any) {
     this.panel.predictionSettings.json = JSON.stringify(net);
-    this.publishAppEvent(AppEvents.alertSuccess, ['File Json Caricato']);
-    this.panel.predictionSettings.predictors = net.Predictors.map((a, index) => {
+    this.panel.predictionSettings.predictors = net.predictors.map((a, index) => {
       return { id: index, name: a };
     });
+    this.publishAppEvent(AppEvents.alertSuccess, ['File Json Caricato']);
+    //TODO: call controller setConfiguration()
   }
 
-  async delete_json_click() {
+  async deleteJsonClick() {
     this.predictionPanelConfig.json = null;
-    this.publishAppEvent(AppEvents.alertSuccess, ['File Json Cancellato']);
     this.panel.predictionSettings.predictors = null;
+    this.publishAppEvent(AppEvents.alertSuccess, ['File Json Cancellato']);
   }
 
-  async update_queries() {
-    this.panel.predictionSettings.query = _.clone(
-      this.queryList.map(a => {
-        return { id: a.refId, name: a.alias };
-      })
-    );
-    // comportamento anomalo: cancella la nodeMap, a causa dell'interfaccia angular
+  async confirmQueries() {
+    // TODO: check that the select does not have the same data
+    const controllerMap = new Map();
+
+    this.panel.predictionSettings.predictors.forEach(predictor => {
+      controllerMap.set(predictor.name, this.panel.predictionSettings.nodeMap[predictor.id]);
+    });
+
+    //TODO: call controller setNodeMap()
+    console.log('BUILDED MAP', controllerMap);
+  }
+
+  updateQueries(dataList) {
+    this.panel.predictionSettings.queries = dataList.map(a => {
+      return { target: a.target };
+    });
   }
 
   onResize() {
@@ -157,8 +168,10 @@ export class PlotlyPanelCtrl extends MetricsPanelCtrl {
   }
 
   onDataReceived(dataList) {
-    this.queryList = dataList;
+    this.updateQueries(dataList);
+    console.log('DATALIST', dataList);
     this.plotlyPanelUtil.plotlyDataReceived(dataList, this.annotationsSrv);
+    //TODO: call controller setDataList()
   }
 
   link(scope, elem, attrs, ctrl) {
