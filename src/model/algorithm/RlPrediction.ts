991b52b0dc9c21c4e6ddaaf2d7341a8b2bd701cf
mrampazz <mrampazz@gmail.com>
2020-04-07 16:19:44 +0200

    Added class attribute in SvmPrediction and RlPrediction

--- src/model/algorithm/RlPrediction.ts
+++ src/model/algorithm/RlPrediction.ts
@@ -6,20 +6,20 @@ import { JsonConfiguration } from '../../types/JsonConfiguration';
 
 export class RlPrediction implements AlgorithmPrediction {
   private writeInflux: WriteInflux;
+  private rl: Regression;
 
-  constructor() {
-  }
+  constructor() {}
 
   predict = (data: DataSet, json: JsonConfiguration, parameters: WriteInfluxParameters): number[][] => {
     // add function in regression library => setCoefficients using json.result for RL
-    const rl: Regression = new Regression();
+    this.rl = new Regression();
     this.writeInflux = new WriteInflux(parameters);
     let result: number[][] = [];
     for (let i = 0; i < data.timestamps.length; i++) {
       for (let j = 0; j < data.data.length; j++) {
         let obj: number[] = data.data[i];
         let x: number = obj[j];
-        result.push(rl.predict(x));
+        result.push(this.rl.predict(x));
       }
     }
     this.writeInflux.writeArrayToInflux(result.flat(), data.timestamps);
--- src/model/algorithm/SvmPrediction.ts
+++ src/model/algorithm/SvmPrediction.ts
@@ -5,22 +5,22 @@ import { WriteInflux } from 'model/writeInflux';
 
 export class SvmPrediction implements AlgorithmPrediction {
   private writeInflux: WriteInflux;
+  private svm: any;
 
-  constructor() {
-  }
+  constructor() {}
 
   // [0,1,2]
   // [[1,2],[2,3],[3,4]]
 
   predict = (data: DataSet, json: SvmJsonConfiguration, parameters: WriteInfluxParameters): number[][] => {
-    const svm: any = new SVM();
+    this.svm = new SVM();
     this.writeInflux = new WriteInflux(parameters);
     let result: number[][] = [];
     // populate array with results
     // json should contains data + labels <- modify ghiotto's library function setData
     for (let i = 0; i < data.timestamps.length; i++) {
-      svm.setData(json);
-      result.push(svm.predict([data.data[i]]));
+      this.svm.setData(json);
+      result.push(this.svm.predict([data.data[i]]));
     }
     this.writeInflux.writeArrayToInflux(result.flat(), data.timestamps);
     return result;
