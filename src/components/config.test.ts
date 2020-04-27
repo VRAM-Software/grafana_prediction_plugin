bead717192d74c0702d589fbb86b9146508691fd
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-04-27 18:45:29 +0200

    Aggiornati test config.ts

--- src/components/config.test.ts
+++ src/components/config.test.ts
@@ -5,55 +5,66 @@
  * Description: Test file for config.ts
  */
 
-import { PluginMeta } from '@grafana/data';
 import { GrafanaPredictionControl } from './config';
 
-describe('test', () => {
-  const injector = {
-    get: () => {
-      return {
-        timeRange: () => {
-          return {
-            from: '',
-            to: '',
-          };
-        },
-      };
-    },
-  };
+const injector = {
+  get: () => {
+    return {
+      timeRange: () => {
+        return {
+          from: '',
+          to: '',
+        };
+      },
+    };
+  },
+};
 
-  const scope = {
-    $on: () => {},
-  };
-  let component: GrafanaPredictionControl;
+const scope = {
+  $on: () => {},
+};
+let component: GrafanaPredictionControl;
 
-  GrafanaPredictionControl.prototype.appEditCtrl = {
-    setPostUpdateHook: x => {},
-  };
+console.log = jest.fn();
 
+GrafanaPredictionControl.prototype.appEditCtrl = {
+  setPostUpdateHook: jest.fn(),
+};
+
+beforeEach(() => {
+  console.log.mockClear();
+  component = new GrafanaPredictionControl(scope, injector);
+});
+
+test('Constructor is called', () => {
+  expect(component.enabled).toBeFalsy();
+  expect(component.appModel.jsonData).toEqual({});
+  expect(typeof component.appModel).toBe('object');
+  expect(console.log).toHaveBeenCalledTimes(1);
+});
+
+test('post update plugin disabled', () => {
+  component.postUpdate();
+  expect(console.log).toHaveBeenCalledTimes(2);
+  expect(console.log).toHaveBeenCalledWith('plugin disabled');
+});
+
+test('post update plugin enabled', () => {
+  component.appModel.enabled = true;
+  component.postUpdate();
+  expect(component.enabled).toBeTruthy();
+  expect(console.log).toHaveBeenCalledTimes(2);
+  expect(console.log).toHaveBeenCalledWith('Post Update, plugin loaded', component);
+});
+
+describe('appModel already initialized', () => {
   beforeEach(() => {
-    console.log = jest.fn();
+    GrafanaPredictionControl.prototype.appModel = {
+      jsonData: 'JJ',
+    };
     component = new GrafanaPredictionControl(scope, injector);
   });
-
-  test('Constructor is called', () => {
-    expect(component.enabled).toBeFalsy();
-    expect(component.appModel.jsonData).toEqual({});
-    expect(typeof component.appModel).toBe('object');
-    expect(console.log).toHaveBeenCalledTimes(1);
-  });
-
-  test('post update plugin disabled', () => {
-    component.postUpdate();
-    expect(console.log).toHaveBeenCalledTimes(2);
-    expect(console.log).toHaveBeenCalledWith('plugin disabled');
-  });
-
-  test('post update plugin enabled', () => {
-    component.appModel.enabled = true;
-    component.postUpdate();
-    expect(component.enabled).toBeTruthy();
-    expect(console.log).toHaveBeenCalledTimes(2);
-    expect(console.log).toHaveBeenCalledWith('Post Update, plugin loaded', component);
+  it('shouldnt initialize new appmodel', () => {
+    expect(component.appModel.jsonData).toEqual('JJ');
   });
 });
